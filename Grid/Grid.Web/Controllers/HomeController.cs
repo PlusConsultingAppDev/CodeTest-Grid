86754ddd58970466632c6690e2ce656721ed4c5c
Alexander Crawford <alxndercrawford@gmail.com>
2017-06-15 21:48:08 -0600

    Web project initial set up.

--- Grid/Grid.Web/Controllers/HomeController.cs
+++ Grid/Grid.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Grid.Domain.Abstract;
+using Grid.Web.Models.Home;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +10,21 @@ namespace Grid.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IRepository repository;
+
+        public HomeController(IRepository repo)
+        {
+            repository = repo;
+        }
+
+
+
         public ActionResult Index()
         {
-            return View();
-        }
+            IndexViewModel viewModel = new IndexViewModel();
+            viewModel.Initialize(repository);
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(viewModel);
         }
     }
 }
---
+++ Grid/Grid.Web/Models/Home/IndexViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Grid.Domain.Abstract;
+using Grid.Domain.Data.Entities;
+using Grid.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Grid.Web.Models.Home
+{
+    public class IndexViewModel
+    {
+        private IRepository repository;
+
+        public List<ProductDto> Products { get; set; }
+
+
+        internal void Initialize(IRepository repo)
+        {
+            repository = repo;
+
+            Products = repository.GetAll<Product>().Select(x => new ProductDto(id: x.Id, 
+                name: x.Name, 
+                color: x.Color, 
+                safetyStockLevel: x.SafetyStockLevel, 
+                reorderPoint: x.ReorderPoint, 
+                standardCost: x.StandardCost, 
+                listPrice: x.ListPrice)).ToList();
+        }
+    }
+}
