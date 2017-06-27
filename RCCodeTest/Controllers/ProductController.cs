83780dfd8b9db53ec5d72c2709b81debc599d335
rcanovic <ralph@canovic.com>
2017-06-27 14:48:41 -0400

    Code Test Solution

---
+++ RCCodeTest/Controllers/ProductController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using RCCodeTest.Dal;
+using RCCodeTest.Models;
+using RCCodeTest.Models.Plugins;
+
+namespace RCCodeTest.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// ProductDataAjaxHandler
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public ActionResult ProductDataAjaxHandler(jQueryDataTableParamModel param)
+        {
+            System.Collections.Generic.IEnumerable<Models.Product> results = null;
+
+            if (Session["ProductResultSet"] == null)
+            {
+                ProductDAL ProdDal = new ProductDAL();
+                results = ProdDal.GetProducts();
+
+                Session["ProductResultSet"] = results;
+            }
+            else
+            {
+                results = ((System.Collections.Generic.IEnumerable<Models.Product>)Session["ProductResultSet"]).ToList();
+            }
+
+            IEnumerable<Models.Product> filteredResults;
+
+            if (!String.IsNullOrEmpty(param.sSearch))
+            {
+                filteredResults = results.Where(x => (!String.IsNullOrEmpty(x.Name) && x.Name.ToLower().Contains(param.sSearch.ToLower())) ||
+                                                     (!String.IsNullOrEmpty(x.ProductNumber) && x.ProductNumber.ToLower().Contains(param.sSearch.ToLower())) ||
+                                                     (!String.IsNullOrEmpty(x.Color) && x.Color.ToLower().Contains(param.sSearch.ToLower())) ||
+                                                     (!String.IsNullOrEmpty(x.SafetyStockLevel.ToString()) && x.SafetyStockLevel.ToString().Contains(param.sSearch.ToLower())) ||
+                                                     (!String.IsNullOrEmpty(x.ReorderPoint.ToString()) && x.ReorderPoint.ToString().Contains(param.sSearch.ToLower())) ||
+                                                     (!String.IsNullOrEmpty(x.StandardCost.ToString()) && x.StandardCost.ToString().Contains(param.sSearch.ToLower())) ||
+                                                     (!String.IsNullOrEmpty(x.ListPrice.ToString()) && x.ListPrice.ToString().Contains(param.sSearch.ToLower()))).ToList();
+            }
+            else
+            {
+                filteredResults = results.ToList();
+            }
+
+            var sortColumnIndex = string.IsNullOrEmpty(Request["iSortCol_0"]) ? 0 : Convert.ToInt32(Request["iSortCol_0"]);
+            Func<Models.Product, string> orderingFunction = (x => sortColumnIndex == 0 ? x.Name :
+                                                                sortColumnIndex == 1 ? x.ProductNumber :
+                                                                sortColumnIndex == 2 ? x.Color :
+                                                                sortColumnIndex == 3 ? x.SafetyStockLevel.ToString() :
+                                                                sortColumnIndex == 4 ? x.ReorderPoint.ToString() :
+                                                                sortColumnIndex == 5 ? x.StandardCost.ToString() :
+                                                                x.ListPrice.ToString());
+
+            var sortDirection = string.IsNullOrEmpty(Request["sSortDir_0"]) ? "asc" : Request["sSortDir_0"]; // asc or desc
+            if (sortDirection == "asc")
+                filteredResults = filteredResults.OrderBy(orderingFunction).ToList();
+            else
+                filteredResults = filteredResults.OrderByDescending(orderingFunction).ToList();
+
+            var displayResults = param.iDisplayLength == -1 ? filteredResults.ToList() : filteredResults.Skip(param.iDisplayStart).Take(param.iDisplayLength).ToList();
+            return Json(new
+            {
+                sEcho = param.sEcho,
+                iTotalRecords = results.ToList().Count(),
+                iTotalDisplayRecords = results.ToList().Count(),
+                iDisplayLength = param.iDisplayLength,
+                aaData = displayResults.ToList()
+            },
+            JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// UpdateData
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        /// <param name="rowId"></param>
+        /// <param name="columnPosition"></param>
+        /// <param name="columnId"></param>
+        /// <param name="columnName"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public string UpdateData(int id, string value, int? rowId, int? columnPosition, int? columnId, string columnName)
+        {
+            try
+            {
+                ProductDAL ProdDal = new ProductDAL();
+                ProdDal.UpdateProduct(id, columnName, value);
+                Session["ProductResultSet"] = null;
+
+                return value;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// AddData
+        /// </summary>
+        /// <param name="ProductId"></param>
+        /// <param name="Name"></param>
+        /// <param name="ProductNumber"></param>
+        /// <param name="Color"></param>
+        /// <param name="SafetyStockLevel"></param>
+        /// <param name="ReorderPoint"></param>
+        /// <param name="StandardCost"></param>
+        /// <param name="ListPrice"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public int AddData(string ProductId, string Name, string ProductNumber, string Color, string SafetyStockLevel, string ReorderPoint, string StandardCost, string ListPrice)
+        {
+            try
+            {
+                ProductDAL ProdDal = new ProductDAL();
+                ProdDal.AddProduct(Name, Color, ProductNumber, SafetyStockLevel, ReorderPoint, StandardCost, ListPrice);
+                Session["ProductResultSet"] = null;
+
+                return 0;
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+    }
+}
---
+++ RCCodeTest/Dal/ProductDAL.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RCCodeTest.Models;
+
+namespace RCCodeTest.Dal
+{
+    public class ProductDAL
+    {
+        /// <summary>
+        /// UpdateProduct
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Models.Product> GetProducts()
+        {
+            using (var productContext = new ProductDataContext())
+            {
+                return (from p in productContext.Products
+                        select new Models.Product
+                        {
+                            ProductID = p.ProductID,
+                            Color = p.Color,
+                            ListPrice = p.ListPrice,
+                            Name = p.Name,
+                            ProductNumber = p.ProductNumber,
+                            ReorderPoint = p.ReorderPoint,
+                            SafetyStockLevel = p.SafetyStockLevel,
+                            StandardCost = p.StandardCost
+                        }).ToList();
+
+            }
+        }
+
+        /// <summary>
+        /// UpdateProduct
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <param name="columnName"></param>
+        /// <param name="newValue"></param>
+        public void UpdateProduct(int productId, string columnName, string newValue)
+        {
+            using (var productContext = new ProductDataContext())
+            {
+                decimal number;
+                Int16 shortNumber;
+                var productToUpdate = (from p in productContext.Products
+                                       where p.ProductID == productId
+                                       select p).FirstOrDefault();
+
+                if (productToUpdate != null)
+                {
+                    switch (columnName)
+                    {
+                        case "Color":
+                            productToUpdate.Color = newValue;
+                            break;
+                        case "List Price":
+                            productToUpdate.ListPrice = (Decimal.TryParse(newValue, out number) ? Convert.ToDecimal(newValue) : productToUpdate.ListPrice);
+                            break;
+                        case "Name":
+                            productToUpdate.Name = newValue;
+                            break;
+                        case "Product Number":
+                            productToUpdate.ProductNumber = newValue;
+                            break;
+                        case "Reorder Point":
+                            productToUpdate.ReorderPoint = (Int16.TryParse(newValue, out shortNumber) ? Convert.ToInt16(newValue) : productToUpdate.ReorderPoint);
+                            break;
+                        case "Safety Stock Level":
+                            productToUpdate.SafetyStockLevel = (Int16.TryParse(newValue, out shortNumber) ? Convert.ToInt16(newValue) : productToUpdate.SafetyStockLevel);
+                            break;
+                        case "Standard Cost":
+                            productToUpdate.StandardCost = (Decimal.TryParse(newValue, out number) ? Convert.ToDecimal(newValue) : productToUpdate.StandardCost);
+                            break;
+                        default:
+                            break;
+
+                    }
+
+                    productContext.SubmitChanges();
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// AddProduct
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="color"></param>
+        /// <param name="productNumber"></param>
+        /// <param name="safetyStockLevel"></param>
+        /// <param name="reorderPoint"></param>
+        /// <param name="standardCost"></param>
+        /// <param name="listPrice"></param>
+        public void AddProduct(string name, string color, string productNumber, string safetyStockLevel, string reorderPoint, string standardCost, string listPrice)
+        {
+            using (var productContext = new ProductDataContext())
+            {
+                Decimal number;
+                Int16 shortNumber;
+
+                Product newProduct = new Product
+                {
+                    Color = color,
+                    ListPrice = (Decimal.TryParse(listPrice, out number) ? Convert.ToDecimal(listPrice) : Decimal.MinValue),
+                    Name = name,
+                    ProductNumber = productNumber,
+                    ReorderPoint = (Int16.TryParse(reorderPoint, out shortNumber) ? Convert.ToInt16(reorderPoint) : Int16.MinValue),
+                    SafetyStockLevel = (Int16.TryParse(safetyStockLevel, out shortNumber) ? Convert.ToInt16(safetyStockLevel) : Int16.MinValue),
+                    StandardCost = (Decimal.TryParse(standardCost, out number) ? Convert.ToDecimal(standardCost) : Decimal.MinValue),
+                    ModifiedDate = DateTime.Now,
+                    SellStartDate = DateTime.Now,
+                    FinishedGoodsFlag = true,
+                    MakeFlag = true,
+                    rowguid = Guid.NewGuid() 
+                };
+
+                productContext.Products.InsertOnSubmit(newProduct);
+                productContext.SubmitChanges();
+            }
+        }
+    }
+}
